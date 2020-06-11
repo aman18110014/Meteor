674fea79d8791a89502892c21cc4b70a7b693425
Aman <meamanlrv@gmail.com>
2020-06-11 08:21:58 +0530

    Changed the collection by implementing for loop

--- my_first_app/imports/api/startup.js
+++ my_first_app/imports/api/startup.js
@@ -2,14 +2,15 @@ import { Mongo } from 'meteor/mongo';
 if(Meteor.isServer){
     Meteor.startup(function(){
         if(Images.find().count()==0){
-            Images.insert(
-                {
-                    img_src: "img_3.jpg",
-                    img_alt: "Image 3",
-                    label: "First image",
-                    desc: "This is my first image"
-                  }
-            );
+            for(var i=1; i<23; i++){
+                Images.insert(
+                    {
+                        img_src: "img_"+i+".jpg",
+                        img_alt: "Image number "+i,
+                      }
+                );
+            }
+            
         }
     });
 }
