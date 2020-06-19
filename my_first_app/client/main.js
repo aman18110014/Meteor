3a8efab92d356a6151dc9f96610877d23490f8a9
Aman <meamanlrv@gmail.com>
2020-06-19 09:52:38 +0530

    Added infinite scroll

--- my_first_app/client/main.js
+++ my_first_app/client/main.js
@@ -5,6 +5,24 @@ import './main.html';
 import { Session } from 'meteor/session';
 
 console.log("I am the client1")
+Session.set("imageLimit", 8);
+
+  lastScrollTop = 0; 
+  $(window).scroll(function(event){
+    // test if we are near the bottom of the window
+    if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+      // where are we in the page? 
+      var scrollTop = $(this).scrollTop();
+      // test if we are going down
+      if (scrollTop > lastScrollTop){
+        // yes we are heading down...
+       Session.set("imageLimit", Session.get("imageLimit") + 4);
+      }
+
+      lastScrollTop = scrollTop;
+    }
+        
+  })
 
 Accounts.ui.config({
   passwordSignupFields: "USERNAME_AND_EMAIL"
@@ -17,7 +35,7 @@ Template.images.helpers({
       return Images.find({createdBy:Session.get("userFilter")}, {sort: {createdOn:-1, rating:-1}});
     }
     else{
-      return Images.find({}, {sort: {createdOn:-1, rating:-1}});
+      return Images.find({}, {sort: {createdOn:-1, rating:-1}, limit:Session.get("imageLimit")});
     }
   },
   filtering_images:function(){
